6a90730c648f15b6fe1b100f88192064b816a6d3
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-09-10 18:30:40 +0300

    Updates

---
+++ precache-manifest.9d54587420dc8a8215fabdef4bd5d4b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd67c6525e5e0ad2a0616e3db3fc725",
+    "url": "/OlegKhodakivskyi/goit-react-hw-06-phonebook-redux/index.html"
+  },
+  {
+    "revision": "ac126b1ca3d795327ca6",
+    "url": "/OlegKhodakivskyi/goit-react-hw-06-phonebook-redux/static/css/main.b619f7cb.chunk.css"
+  },
+  {
+    "revision": "116c19054d25b0762373",
+    "url": "/OlegKhodakivskyi/goit-react-hw-06-phonebook-redux/static/js/2.14f70b71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/OlegKhodakivskyi/goit-react-hw-06-phonebook-redux/static/js/2.14f70b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac126b1ca3d795327ca6",
+    "url": "/OlegKhodakivskyi/goit-react-hw-06-phonebook-redux/static/js/main.b700cd8d.chunk.js"
+  },
+  {
+    "revision": "c95bd704897acb732789",
+    "url": "/OlegKhodakivskyi/goit-react-hw-06-phonebook-redux/static/js/runtime-main.3a0333a0.js"
+  }
+]);
